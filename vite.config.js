0f7df668a2cbc3a4fd9aa36d5fd3e7dd8f09b39d
Saumymishra <saumymishra08@gmail.com>
2025-06-28 21:40:59 +0530

    fixed isssue

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Portfolio/',   // Important for GitHub Pages path!
+  base: '/Myportfolio/',   // Important for GitHub Pages path!
   plugins: [react()],
 });
